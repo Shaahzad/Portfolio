2fd488e7a921046dd8e40cbf2d6e6ff7da3ea527
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-06-13 15:03:03 +0500

    Done

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
           (MongoDB, Express.js, React.js, Node.js).
           Eager to leverage technical skills and problem-solving abilities to contribute to the development of innovative
            web applications within a collaborative team environment. Possesses excellent communication, critical thinking,
-           and learning agility to thrive in a fast-paced technological landscape.pen_spark</p>
+           and learning agility to thrive in a fast-paced technological landscape.</p>
            <div>
             <p className='follow'>Follow And Connects With Me</p>
            <a href="https://github.com/shaahzad" target="_blank">
