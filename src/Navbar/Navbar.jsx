78bad67a6a69dfa31f1f271e71da5dc1562b7c12
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-08-03 14:16:08 +0500

    DOne

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Navbar.css"
-import Resume from "/images/Muhammad Shahzad.pdf"
+import Resume from "/images/Muhammad Shahzad resume.pdf"
 
 const Navbar = () => {
   return (
