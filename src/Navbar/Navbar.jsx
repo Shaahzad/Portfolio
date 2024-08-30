39260ee28c50b84ac8546d9edf9b4d3b2e0ec48a
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-08-30 19:24:06 +0500

    Done

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./Navbar.css"
-import Resume from "../../public/images/Muhammad Shahzad.pdf"
+import Resume from "../../public/images/Muhammad Shahzad Mern.pdf"
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <h1 className='fname'>Muhammad <span className='name'>Shahzad</span></h1>
       <button>
-        <a href={Resume} download="Muhammad Shahzad.pdf">
+        <a href={Resume} download="Muhammad Shahzad Mern.pdf">
         Download Resume
         </a>
       </button>
