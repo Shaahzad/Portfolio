c296e7fe236fcfdd48b0b8e968336fde97f726ff
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-01-08 22:33:05 +0500

    Done

--- src/skills/Skill.jsx
+++ src/skills/Skill.jsx
@@ -21,47 +21,47 @@ const Skill = () => {
     <div className='px-8 mt-10 mb-20'> 
   <h1 className='text-center text-3xl font-bold text-white mb-2'>Frontend Skills</h1>
     <div className='flex flex-wrap justify-center gap-5'>
-            <div className='skill btn btn-outline btn-info'>
+            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
                 <h3>HTML</h3> 
                 <FaHtml5 style={{color:"brown", fontSize:"18px"}}/>
             </div>
-            <div className='skill btn btn-outline btn-info'>
+            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
                 <h3>CSS</h3>
                 <FaCss3 style={{color:"blue", fontSize:"18px" }}/>
             </div>
-            <div className='skill btn btn-outline btn-info'> 
+            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'> 
                 <h3>JavaScript</h3>
                 <FaJs style={{color:"yellow", fontSize:"18px"}}/>
             </div>
-            <div className='skill btn btn-outline btn-info'>
+            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
                 <h3>React</h3>
                 <FaReact style={{color:"lightblue",fontSize:"18px"}}/>
             </div>
-            <div className='skill btn btn-outline btn-info'>
+            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
                 <h3>Bootstrap</h3>
                 <FaBootstrap style={{color:"purple", fontSize:"18px"}}/>
             </div>
-            <div className='skill btn btn-outline btn-info'>
+            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
                 <h3>Tailwind</h3>
                 <RiTailwindCssFill style={{color:"skyblue", fontSize:"18px"}}/>
             </div>
-            <div className='skill btn btn-outline btn-info'>
+            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
               <h3>Redux ToolKit</h3>
               <BiLogoRedux style={{color:"purple", fontSize:"18px"}}/>
             </div>
-            <div className='skill btn btn-outline btn-info'>
+            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
               <h3>Github</h3>
               <FaGithub/>
             </div>
-             <div className="skill btn btn-outline btn-info">
+             <div className="skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500">
               <h3>Firebase</h3>
               <RiFirebaseFill style={{color:"yellow", fontSize:"20px"}}/>
             </div> 
-            <div className="skill btn btn-outline btn-info">
+            <div className="skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500">
               <h3>MUI</h3>
               <SiMui style={{color:"blue", fontSize:"20px"}}/>
             </div>
-            <div className="skill btn btn-outline btn-info">
+            <div className="skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500">
               <h3>React Native</h3>
               <TbBrandReactNative style={{color:"blue", fontSize:"20px"}}/>
             </div>
@@ -69,15 +69,15 @@ const Skill = () => {
         <div className='px-8 mt-12 mb-20'>
     <h1 className='text-center text-3xl font-bold text-white mb-2'>Backend Skills</h1>
     <div className='flex flex-wrap justify-center gap-5'>
-        <div className="skill btn btn-outline btn-info">
+        <div className="skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500">
             <h3>Nodejs</h3>
             <FaNodeJs style={{color:"green", fontSize:"20px"}}/>
         </div>
-        <div className="skill btn btn-outline btn-info">
+        <div className="skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500">
             <h3>Express.Js</h3>
             <SiExpress style={{fontSize:"20px"}}/>
         </div>
-        <div className="skill btn btn-outline btn-info">
+        <div className="skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500">
             <h3>MongoDb</h3>
             <DiMongodb style={{color:"green", fontSize:"20px"}}/>
         </div>
