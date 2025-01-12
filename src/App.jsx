736c67ecba07f539a5e4dda0110f247b15c7876a
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-01-12 22:59:18 +0500

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
-import Project from './Projects/Project'
-import About from './About/About'
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
-import Contact from './contact/Contact'
-import Footer from './footer/Footer'
-import Skill from './skills/Skill'
 import Home from './Home/Home'
 
 
@@ -14,11 +9,6 @@ const App = () => {
     <div>
       <BrowserRouter>
       <Navbar/>
-       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/skill' element={<Skill/>} />
-       </Routes>
-      <Footer/>
       </BrowserRouter>
     </div>
   )
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,40 +1,8 @@
-import { DrawerBackdrop, DrawerRoot } from '@/components/ui/drawer';
 import React from 'react'
 import { FaHamburger } from "react-icons/fa";
 
 
 const Navbar = () => {
-
-  const HandleSideBar = () => {
-      return (
-        <DrawerRoot>
-          <DrawerBackdrop />
-          <DrawerTrigger asChild>
-            <Button variant="outline" size="sm">
-              Open Drawer
-            </Button>
-          </DrawerTrigger>
-          <DrawerContent>
-            <DrawerHeader>
-              <DrawerTitle>Drawer Title</DrawerTitle>
-            </DrawerHeader>
-            <DrawerBody>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </DrawerBody>
-            <DrawerFooter>
-              <DrawerActionTrigger asChild>
-                <Button variant="outline">Cancel</Button>
-              </DrawerActionTrigger>
-              <Button>Save</Button>
-            </DrawerFooter>
-            <DrawerCloseTrigger />
-          </DrawerContent>
-        </DrawerRoot>
-      )
-  }
   return (
     <div className='flex justify-between items-center px-8 py-4 bg-purple-400'>
       <h1 className='text-3xl font-bold text-white'>Navbar</h1>
@@ -45,7 +13,7 @@ const Navbar = () => {
           <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer'>Projects</li>
           <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer'>Skills</li>
         </ul>
-          <FaHamburger size={25} className='flex md:hidden' onClick={HandleSideBar}/>
+          <FaHamburger size={25} className='flex md:hidden'/>
       </div>
   )
 }
