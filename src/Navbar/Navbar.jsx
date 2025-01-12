944e1baffb8bc3ae3343ec3c545872b15d91a6a3
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-01-12 14:21:45 +0500

    Done

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,32 +1,18 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from 'react'
 
-export default function ButtonAppBar() {
+const Navbar = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-          <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    <div className='flex justify-between items-center px-8 py-4 bg-purple-400'>
+      <h1 className='text-3xl font-bold text-white' >Navbar</h1>
+        <ul className='flex items-center gap-4 text-white font-bold'>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer'>Home</li>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer'>About</li>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer'>Contact</li>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer'>Projects</li>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer'>Skills</li>
+        </ul>
+      </div>
+  )
 }
+
+export default Navbar
