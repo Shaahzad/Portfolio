03d5b48ac09df5d8e68d963a9cc100a39b768433
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-01-13 08:55:55 +0500

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
-import Home from './Home/Home'
 
 
 const App = () => {
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -5,15 +5,15 @@ import { FaHamburger } from "react-icons/fa";
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center px-8 py-4 bg-purple-400'>
-      <h1 className='text-3xl font-bold text-white'>Navbar</h1>
+      <h1 className='text-3xl font-bold text-white'>Muhammad Shahzad</h1>
         <ul className='md:flex items-center gap-4 text-white font-bold hidden'>
-          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer'>Home</li>
-          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer'>About</li>
-          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer'>Contact</li>
-          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer'>Projects</li>
-          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer'>Skills</li>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer transition ease-in-out delay-150'>Home</li>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer transition ease-in-out delay-150'>About</li>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer transition ease-in-out delay-150'>Contact</li>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer transition ease-in-out delay-150'>Projects</li>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer transition ease-in-out delay-150'>Skills</li>
         </ul>
-          <FaHamburger size={25} className='flex md:hidden'/>
+          <FaHamburger size={25} className='flex md:hidden cursor-pointer' />
       </div>
   )
 }
