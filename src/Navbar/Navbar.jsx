eeb693fcd1b607ef593c2fc86ef85c0bcf507779
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-01-13 09:58:31 +0500

    Done

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { FaHamburger } from "react-icons/fa";
+import React, { useState } from 'react'
+import Sidebar from '../Sidebar/Sidebar';
 
 
 const Navbar = () => {
   return (
+    <>
     <div className='flex justify-between items-center px-8 py-4 bg-purple-400'>
       <h1 className='text-3xl font-bold text-white'>Muhammad Shahzad</h1>
         <ul className='md:flex items-center gap-4 text-white font-bold hidden'>
@@ -13,8 +14,9 @@ const Navbar = () => {
           <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer transition ease-in-out delay-150'>Projects</li>
           <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer transition ease-in-out delay-150'>Skills</li>
         </ul>
-          <FaHamburger size={25} className='flex md:hidden cursor-pointer' />
+        <Sidebar/>
       </div>
+    </>
   )
 }
 
--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { FaHamburger } from "react-icons/fa";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet.jsx";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+  <Sheet>
+  <SheetTrigger className='md:hidden'>
+  <FaHamburger size={25} className='flex md:hidden cursor-pointer'/>
+  </SheetTrigger>
+  <SheetContent>
+    <SheetHeader className='mt-40'>
+      <ul className='flex flex-col justify-center items-center gap-4 text-black font-bold'>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer transition ease-in-out delay-150'>Home</li>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer transition ease-in-out delay-150'>About</li>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer transition ease-in-out delay-150'>Contact</li>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer transition ease-in-out delay-150'>Projects</li>
+          <li className='hover:bg-gray-300 rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer transition ease-in-out delay-150'>Skills</li>
+        </ul>
+    </SheetHeader>
+  </SheetContent>
+</Sheet>
   )
 }
 
