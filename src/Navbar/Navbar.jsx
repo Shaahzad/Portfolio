2fb88338b1e2d176e6b7194ec2c46b46cac98e85
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-01-15 23:06:26 +0500

    Done

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -7,7 +7,9 @@ const Navbar = () => {
   return (
     <>
     <div className='flex justify-between items-center px-12 py-4 bg-purple-400'>
+      <Link to="/">
       <h1 className='text:xl md:text-3xl font-bold text-white'>Muhammad Shahzad</h1>
+      </Link>
         <ul className='md:flex items-center gap-16 text-white font-bold hidden'>
           <li className='hover:bg-gray-300 text-xl rounded px-2 py-2 hover:px-2 hover:py-2 cursor-pointer transition ease-in-out delay-150'>
             <Link to="/">
--- src/skills/Skill.jsx
+++ src/skills/Skill.jsx
@@ -18,68 +18,69 @@ import { TbBrandReactNative } from "react-icons/tb";
 const Skill = () => {
   return (
     <>
-    <div className='px-8 mt-12 mb-20'> 
-  <h1 className='text-center text-3xl font-bold text-white mb-2'>Frontend Skills</h1>
+  <div className='px-8 mt-12 mb-20'> 
+  <image src="../../public/images/skill.jpg"/>
+  <h1 className='text-center text-3xl font-bold text-black mb-2'>Frontend Skills</h1>
     <div className='flex flex-wrap justify-center gap-5'>
-            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
-                <h3>HTML</h3> 
-                <FaHtml5 style={{color:"brown", fontSize:"18px"}}/>
+    <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+                <h3 className='font-bold'>HTML</h3> 
+                <FaHtml5 style={{color:"brown", fontSize:"30px"}}/>
             </div>
-            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
-                <h3>CSS</h3>
-                <FaCss3 style={{color:"blue", fontSize:"18px" }}/>
+            <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+                <h3 className='font-bold'>CSS</h3>
+                <FaCss3 style={{color:"blue", fontSize:"30px" }}/>
             </div>
-            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'> 
-                <h3>JavaScript</h3>
-                <FaJs style={{color:"yellow", fontSize:"18px"}}/>
+            <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+                <h3 className='font-bold'>JavaScript</h3>
+                <FaJs style={{color:"yellow", fontSize:"30px"}}/>
             </div>
-            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
-                <h3>React</h3>
-                <FaReact style={{color:"lightblue",fontSize:"18px"}}/>
+            <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+                <h3 className='font-bold'>React</h3>
+                <FaReact style={{color:"lightblue", fontSize:"30px"}}/>
             </div>
-            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
-                <h3>Bootstrap</h3>
-                <FaBootstrap style={{color:"purple", fontSize:"18px"}}/>
+            <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+                <h3 className='font-bold'>Bootstrap</h3>
+                <FaBootstrap style={{color:"purple", fontSize:"30px"}}/>
             </div>
-            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
-                <h3>Tailwind</h3>
-                <RiTailwindCssFill style={{color:"skyblue", fontSize:"18px"}}/>
+            <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+                <h3 className='font-bold'>Tailwind</h3>
+                <RiTailwindCssFill style={{color:"skyblue", fontSize:"30px"}}/>
             </div>
-            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
-              <h3>Redux ToolKit</h3>
-              <BiLogoRedux style={{color:"purple", fontSize:"18px"}}/>
+            <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+              <h3 className='font-bold'>Redux ToolKit</h3>
+              <BiLogoRedux style={{color:"purple", fontSize:"30px"}}/>
             </div>
-            <div className='skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500'>
-              <h3>Github</h3>
-              <FaGithub/>
+            <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+              <h3 className='font-bold'>Github</h3>
+              <FaGithub style={{fontSize: "30px"}}/>
             </div>
-             <div className="skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500">
-              <h3>Firebase</h3>
-              <RiFirebaseFill style={{color:"yellow", fontSize:"20px"}}/>
+            <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+              <h3 className='font-bold'>Firebase</h3>
+              <RiFirebaseFill style={{color:"yellow", fontSize:"30px"}}/>
             </div> 
-            <div className="skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500">
-              <h3>MUI</h3>
-              <SiMui style={{color:"blue", fontSize:"20px"}}/>
+            <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+              <h3 className='font-bold'>MUI</h3>
+              <SiMui style={{color:"blue", fontSize:"30px"}}/>
             </div>
-            <div className="skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500">
-              <h3>React Native</h3>
-              <TbBrandReactNative style={{color:"blue", fontSize:"20px"}}/>
+            <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+              <h3 className='font-bold'>React Native</h3>
+              <TbBrandReactNative style={{color:"blue", fontSize:"30px"}}/>
             </div>
         </div>
         <div className='px-8 mt-12 mb-20'>
-    <h1 className='text-center text-3xl font-bold text-white mb-2'>Backend Skills</h1>
+    <h1 className='text-center text-3xl font-bold text-black mb-2'>Backend Skills</h1>
     <div className='flex flex-wrap justify-center gap-5'>
-        <div className="skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500">
-            <h3>Nodejs</h3>
-            <FaNodeJs style={{color:"green", fontSize:"20px"}}/>
+        <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+            <h3 className='font-bold'>Nodejs</h3>
+            <FaNodeJs style={{color:"green", fontSize:"30px"}}/>
         </div>
-        <div className="skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500">
-            <h3>Express.Js</h3>
-            <SiExpress style={{fontSize:"20px"}}/>
+        <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+            <h3 className='font-bold'>Express.Js</h3>
+            <SiExpress style={{fontSize:"30px", color: "white"}}/>
         </div>
-        <div className="skill btn btn-outline bg-gradient-to-r from-purple-500 via-blue-500 to-green-500">
-            <h3>MongoDb</h3>
-            <DiMongodb style={{color:"green", fontSize:"20px"}}/>
+        <div className="flex items-center gap-2 cursor-pointer hover:scale-110 bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 px-6 py-2 rounded">
+            <h3 className='font-bold'>MongoDb</h3>
+            <DiMongodb style={{color:"green", fontSize:"30px"}}/>
         </div>
     </div>
         </div>
